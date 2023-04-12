51aa86ee32578ae3cc80fa83dae19de6b8a96898
humaidokasha@gmail.com <humaidokasha@gmail.com>
2023-04-13 01:16:39 +0500

    commit my work

--- src/app/private/examine-thoughts/examine-thoughts7/examine-thoughts7.component.ts
+++ src/app/private/examine-thoughts/examine-thoughts7/examine-thoughts7.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { LocalCacheService } from 'src/app/services/local-cache.service';
+import { ModuleService } from 'src/app/services/module.service';
 
 @Component({
   selector: 'app-examine-thoughts7',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamineThoughts7Component implements OnInit {
 
-  constructor() { }
+  diary:any ={};
+  currentUser: any;
+  constructor(private moduleService: ModuleService, private localService: LocalCacheService,private router:Router) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.currentUser = this.localService.getCurrentUser();
+    this.diary.uid = this.currentUser.id;
+  }
+  
+  async addThoughtDairy() {
+    await this.moduleService.addThoughtDairy(this.diary).toPromise().then((res: any) => {
+      if (res.success) {
+        this.router.navigate(['../examinethoughts/examine8']);
+      }
+    })
+  }
 }
--- src/app/services/module.service.ts
+++ src/app/services/module.service.ts
@@ -8,15 +8,22 @@ import { environment } from 'src/environments/environment';
 })
 export class ModuleService {
 
-  constructor(private http: HttpClient ) { }
+  constructor(private http: HttpClient) { }
 
-  public addModule(module:any):Observable<any> {
-   return  this.http.post(environment.BASE_URL+'module',module);
+  public addModule(module: any): Observable<any> {
+    return this.http.post(environment.BASE_URL + 'module', module);
   }
 
-  public getModuleScoreById(id:any,module:any):Observable<any> {
-    return  this.http.get(environment.BASE_URL+'module/score/'+id+'/'+module);
-   }
- 
+  public getModuleScoreById(id: any, module: any): Observable<any> {
+    return this.http.get(environment.BASE_URL + 'module/score/' + id + '/' + module);
+  }
+  
+  public addThoughtDairy(diary: any): Observable<any> {
+    return this.http.post(environment.BASE_URL + 'module/addThoughtDairy', diary);
+  }
+
+  public getThoughtDiaryById(id: any): Observable<any> {
+    return this.http.get(environment.BASE_URL + 'module/thoughtDiary/' + id);
+  }
 
 }
