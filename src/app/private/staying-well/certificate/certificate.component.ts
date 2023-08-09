5dff8ab260d2471e9eec3e36918ba75af643ea4f
humaidokasha@gmail.com <humaidokasha@gmail.com>
2023-08-09 23:38:04 +0500

    commit

--- src/app/private/staying-well/certificate/certificate.component.ts
+++ src/app/private/staying-well/certificate/certificate.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import jsPDF from 'jspdf';
+// import pdfMake from 'pdfMake/build/pdfMake.js';
+// import htmlToPdfmake from 'html-to-pdfmake';
 @Component({
   selector: 'app-certificate',
   templateUrl: './certificate.component.html',
   styleUrls: ['./certificate.component.scss']
 })
 export class CertificateComponent {
-
+  title = 'htmltopdf';
+  
+  @ViewChild('pdfTable',{static:false}) pdfTable!: ElementRef;
+  
+  // public downloadAsPDF() {
+  //   const doc = new jsPDF();
+   
+  //   const pdfTable = this.pdfTable.nativeElement; 
+   
+  //   var html = htmlToPdfmake(pdfTable.innerHTML);
+     
+  //   const documentDefinition = { content: html };
+  //   pdfMake.createPdf(documentDefinition).open(); 
+     makePdf(){
+      let pdf= new jsPDF('p','pt','a4')
+      pdf.html(this.pdfTable.nativeElement,{
+        callback:(pdf)=>{
+          pdf.save("sample.pdf")
+        }
+      })
+     }
 }
+
