01cfb04bf8f9f7ac0ef0d883a846ff49fa3eb01a
humaidokasha@gmail.com <humaidokasha@gmail.com>
2023-08-20 15:18:27 +0500

    commit

---
+++ src/app/shared/svg-dashboard/svg-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-dashboard',
+  templateUrl: './svg-dashboard.component.html',
+  styleUrls: ['./svg-dashboard.component.scss']
+})
+export class SvgDashboardComponent {
+
+}
